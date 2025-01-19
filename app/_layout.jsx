56ae5e4fdb520c1a4d2512bfaf041883298bf70b
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-01-19 07:32:03 +0600

    functoins

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,5 +2,11 @@ import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </SafeAreaProvider>
+  );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,40 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, TextInput, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+import { data } from "../data/todos";
 
 export default function Index() {
+  const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    if (text.trim()) {
+      const newId = todos.length > 0 ? todos[0].id + 1 : 1;
+      setTodos([{ id: newId, title: text, completed: false }, ...todos]);
+      setText("");
+    }
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <TextInput />
+        <Pressable>
+          <Text></Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
